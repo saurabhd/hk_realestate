6903aa20f061df3e261de24c65ca0039fa6d7607
root <darshi.shah@addwebsolution.com>
2016-04-08 17:12:03 +0530

    removed folders that had .git in them

--- sites/all/themes/hk_theme/templates/html--colorbox.tpl.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-  <head profile="<?php print $grddl_profile; ?>">
-    <title><?php print $head_title; ?></title>
-  </head>
-  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-    <?php print $page; ?>
-  </body>
-</html>
--- sites/all/themes/hk_theme/templates/page--colorbox.tpl.php
+++
@@ -1,6 +0,0 @@
-        <!-- page colorbox -->
-        <?php if ($title): ?>
-          <h1 class="title" id="page-title"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <!-- /page colorbox -->

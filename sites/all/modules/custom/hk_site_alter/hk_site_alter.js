f61ddcaf8fa924985641a5cfb890e2d12bf169ca
vivek-addweb <vivek.addweb@gmail.com>
2016-05-03 11:33:08 +0530

    #5 - Add a possibility to choose one or more apartments fom the list

---
+++ sites/all/modules/custom/hk_site_alter/hk_site_alter.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Javascript behaviors for hk theme bootstrap integrations.
+ */
+
+(function (jQuery) {
+
+  Drupal.behaviors.hk_site_alter = {
+	attach: function (context, settings) {
+
+   var data = [];
+	jQuery( ".page-favorites .Inquiry-button" ).click(function(n) {
+	  data = [];
+    jQuery( "#views-form-merkliste-page .views-row .inquiry" ).each(function() {
+     if(this.checked){
+    	data.push(jQuery(this).val());
+     }
+    });
+		window.location.href = '/anfrage?field_angefragte_wohnung='+data+'&destination=/favorites&width=600&height=600';
+	 });
+	}
+  };
+}(jQuery));

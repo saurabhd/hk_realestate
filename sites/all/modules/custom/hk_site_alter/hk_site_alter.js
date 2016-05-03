68456c7fa22a60cc20bf9b89f3b91f5345d72185
root <darshi.shah@addwebsolution.com>
2016-05-03 11:38:26 +0530

    #5 js

--- sites/all/modules/custom/hk_site_alter/hk_site_alter.js
+++ sites/all/modules/custom/hk_site_alter/hk_site_alter.js
@@ -1,23 +1,27 @@
 /**
+/*
+
  * @file
+
  * Javascript behaviors for hk theme bootstrap integrations.
+
  */
 
 (function (jQuery) {
-
   Drupal.behaviors.hk_site_alter = {
-	attach: function (context, settings) {
-
+    attach: function (context, settings) {
    var data = [];
-	jQuery( ".page-favorites .Inquiry-button" ).click(function(n) {
-	  data = [];
+    jQuery( ".page-favorites .Inquiry-button" ).click(function(n) {
+      data = [];
     jQuery( "#views-form-merkliste-page .views-row .inquiry" ).each(function() {
      if(this.checked){
-    	data.push(jQuery(this).val());
+        data.push(jQuery(this).val());
      }
     });
-		window.location.href = '/anfrage?field_angefragte_wohnung='+data+'&destination=/favorites&width=600&height=600';
-	 });
-	}
+    if (data.length != 0) {
+          window.location.href = '/anfrage?field_angefragte_wohnung='+data+'&destination=/favorites&width=600&height=600';
+       }
+   });
+    }
   };
 }(jQuery));

42db8cdae8f55d739f92e73711b60f0dda09b67b
vivek-addweb <vivek.addweb@gmail.com>
2016-05-05 16:30:12 +0530

    #6 - js and design

--- sites/all/modules/custom/hk_site_alter/hk_site_alter.js
+++ sites/all/modules/custom/hk_site_alter/hk_site_alter.js
@@ -1,27 +1,31 @@
 /**
-/*
-
  * @file
-
  * Javascript behaviors for hk theme bootstrap integrations.
-
  */
 
 (function (jQuery) {
+
   Drupal.behaviors.hk_site_alter = {
-    attach: function (context, settings) {
-   var data = [];
-    jQuery( ".page-favorites .Inquiry-button" ).click(function(n) {
-      data = [];
-    jQuery( "#views-form-merkliste-page .views-row .inquiry" ).each(function() {
+  attach: function (context, settings) {
+    var data = [];
+    var alldata = [];
+   jQuery( ".page-favorites .Inquiry-button" ).click(function(e) {
+    data = [];
+    jQuery( ".view-id-merkliste .views-row .inquiry" ).each(function() {
      if(this.checked){
-        data.push(jQuery(this).val());
+      data.push(jQuery(this).val());
      }
     });
+    alldata = [];
+    jQuery( ".view-id-merkliste .views-row .inquiry" ).each(function() {
+      alldata.push(jQuery(this).val());
+    });
     if (data.length != 0) {
-          window.location.href = '/anfrage?field_angefragte_wohnung='+data+'&destination=/favorites&width=600&height=600';
-       }
+       jQuery.colorbox({width:"900px", height:"600px", href:'/anfrage?field_angefragte_wohnung='+data+'&destination=/favorites&width=600&height=600'});
+     }else{
+       jQuery.colorbox({width:"900px", height:"600px", href:'/anfrage?field_angefragte_wohnung='+alldata+'&destination=/favorites&width=600&height=600'});
+     }
    });
-    }
+  }
   };
 }(jQuery));

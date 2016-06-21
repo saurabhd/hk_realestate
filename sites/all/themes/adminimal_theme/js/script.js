c591edd465f8f63f01dfe1e53d05c7661a1cab77
darshi-shah <darshi.shah@addwebsolution.com>
2016-06-21 20:35:36 +0530

    add rule for activity status change

---
+++ sites/all/themes/adminimal_theme/js/script.js
@@ -0,0 +1,74 @@
+/*
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+(function ($, Drupal, window, document, undefined) {
+
+(function ($) {
+  Drupal.behaviors.BearScripts = {
+    attach: function (context, settings) {
+
+      $(".views-field-field-activity-notes").click(function(){
+          $(this).parent().next().slideToggle();
+      });
+
+      $('.facetapi-multiselect-field-bezirk').select2({
+          placeholder: Drupal.t('Borough'),
+          placeholderOption: "first"
+      });
+
+      $('.facetapi-multiselect-field-kategorie').select2({
+          placeholder: Drupal.t('Category'),
+          placeholderOption: "first"
+      });
+
+      $('.preis select').select2({
+          placeholder: Drupal.t('Price'),
+          placeholderOption: "first"
+      });
+      $('.zimmer select').select2({
+          placeholder: Drupal.t('Rooms'),
+          placeholderOption: "first"
+      });
+      $('.flaeche select').select2({
+          placeholder: Drupal.t('Space (ca.)'),
+          placeholderOption: "first"
+      });
+
+      $('.bedrooms select').select2({
+          placeholder: Drupal.t('Bedrooms'),
+          placeholderOption: "first"
+      });
+
+      $('#facetapi-bonus-select-field-apart-bathrooms select').select2({
+          placeholder: Drupal.t('Bathrooms'),
+          placeholderOption: "first"
+      });
+
+      /*$('#facetapi-bonus-select-field-apart-type select').select2({
+          placeholder: Drupal.t('Type'),
+          placeholderOption: "first"
+      });
+
+      $('#facetapi-bonus-select-field-apart-floor select').select2({
+          placeholder: Drupal.t('Floor'),
+          placeholderOption: "first"
+      });
+
+      $('#facetapi-bonus-select-field-status-quo select').select2({
+          placeholder: Drupal.t('Status quo'),
+          placeholderOption: "first"
+      });
+
+      $('#facetapi-bonus-select-field-ausstattung select').select2({
+          placeholder: Drupal.t('Fittings'),
+          placeholderOption: "first"
+      });*/
+    }
+  }
+}(jQuery));
+
+})(jQuery, Drupal, this, this.document);

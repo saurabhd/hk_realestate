6543e3bcdb2b717cc4a9881c7aa1e1852770b546
darshi-shah <darshi.shah@addwebsolution.com>
2016-07-07 15:16:14 +0530

    add flag changes for task

--- sites/all/themes/hk_adminimal_theme/templates/views-view-unformatted--latest-activity-by-contact--block.tpl.php
+++ sites/all/themes/hk_adminimal_theme/templates/views-view-unformatted--latest-activity-by-contact--block.tpl.php
@@ -12,7 +12,50 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row):?>
-  <td class="views-field views-field-activity-date"><?php print $row['field_activity_date']; ?></td>
+<?php
+$date = $row['field_activity_date'];
+$time_ago = strtotime($date);
+$cur_time   = time();
+$time_elapsed   = $cur_time - $time_ago;
+$days     = round($time_elapsed / 86400 );
+$weeks    = round($time_elapsed / 604800);
+$months   = round($time_elapsed / 2600640 );
+$years    = round($time_elapsed / 31207680 );
+$left_time = '';
+ //Days
+ if($days <= 7){
+   if($days==1){
+     $left_time =  "yesterday";
+   }else{
+     $left_time =  $days." days Before";
+   }
+ }
+ //Weeks
+ else if($weeks <= 4.3){
+   if($weeks==1){
+     $left_time =  "1 week Before";
+   }else{
+     $left_time =  $weeks." weeks Before";
+   }
+ }
+ //Months
+ else if($months <=12){
+   if($months==1){
+     $left_time =  "1 month Before";
+   }else{
+     $left_time =  $months." months Before";
+   }
+ }
+ //Years
+ else{
+   if($years==1){
+     $left_time =  "1 year Before";
+   }else{
+     $left_time =  $years." years Before";
+   }
+ }
+?>
+  <td class="views-field views-field-activity-date"><?php print $left_time; ?></td>
   <td class="views-field views-field-activity-type"><?php print $row['type']; ?><span class="plus-icon"></span></td>
   <td class="views-field views-field-action"><?php print $row['nothing']; ?></td>
   <?php if(isset($row['field_activity_notes']) && !empty($row['field_activity_notes'])) { ?>

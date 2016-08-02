08d53889b33d876de6b82cb32d8962fff7e9e2c1
root <darshi.shah@addwebsolution.com>
2016-08-02 11:37:02 +0530

    #29 - add service menu for telephone number

--- sites/all/themes/hk_theme/js/script.js
+++ sites/all/themes/hk_theme/js/script.js
@@ -212,6 +212,13 @@
           jQuery('.facet-select').parent().removeClass('active');
       });
       // End //
+
+      // Hover Phone number on hover of service menu //
+      jQuery("#block-menu-menu-service-menu ul.menu li .icon-hk-icon-telephone").parent().before('<div class="menu-tooltip" style="display:none">+49 30 780 870 80</div>');
+      jQuery("#block-menu-menu-service-menu ul.menu li:nth-child(2) a").on("click", function(e) {
+        e.preventDefault();
+      });
+      // End //
     }
 
   }

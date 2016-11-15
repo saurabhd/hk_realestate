6d7a5fe2aaa04c8b3ac7a7704cc4574d7aad0bc8
heliogabal <rhalbmann@gmail.com>
2016-11-15 11:03:57 +0100

    Update local.settings.php
    need to deactivate the mobile switch cache if we disabled the module.

--- sites/addweb.dev.herbert-kohlmeyer.de/local.settings.php
+++ sites/addweb.dev.herbert-kohlmeyer.de/local.settings.php
@@ -4,5 +4,5 @@ $conf['feeds_process_limit'] = 2000;
 $conf['image_allow_insecure_derivatives'] = TRUE;
 $conf['image_suppress_itok_output'] = TRUE;
 
-$conf['cache_backends'][] = 'sites/all/modules/contrib/mobile_switch/mobile_switch_database_cache.inc';
-$conf['cache_class_cache_page'] = 'MobileSwitchDatabaseCache';
+#$conf['cache_backends'][] = 'sites/all/modules/contrib/mobile_switch/mobile_switch_database_cache.inc';
+#$conf['cache_class_cache_page'] = 'MobileSwitchDatabaseCache';
